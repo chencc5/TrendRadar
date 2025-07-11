e4fb73ae91a92ca9d2e03369e6e4bf8fd67314f1
Ellchan <chencc5@qq.com>
2025-07-12 00:20:29 +0800

    更新main.py

--- main.py
+++ main.py
@@ -2585,17 +2585,9 @@ class NewsAnalyzer:
             print("运行模式: 正常模式（频率词统计 + 新增检测）")
 
         ids = [
-            ("toutiao", "今日头条"),
-            ("baidu", "百度热搜"),
             ("wallstreetcn-hot", "华尔街见闻"),
-            ("thepaper", "澎湃新闻"),
-            ("bilibili-hot-search", "bilibili 热搜"),
             ("cls-hot", "财联社热门"),
             ("ifeng", "凤凰网"),
-            "tieba",
-            "weibo",
-            "douyin",
-            "zhihu",
             # 新增数据源
             ("coolapk", "酷安"),
             ("v2ex", "V2EX"),
